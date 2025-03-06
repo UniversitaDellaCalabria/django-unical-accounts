510f323ed73bb379216b2c13c8cddcd796b417b4
Francesco Filicetti <francesco.filicetti@unical.it>
2025-03-06 11:21:02 +0100

    fix: change ugettext_lazy (deprecated) in gettext_lazy

--- unical_accounts/models.py
+++ unical_accounts/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext as _
 from django.contrib.contenttypes.models import ContentType
 
 from django.conf import settings
